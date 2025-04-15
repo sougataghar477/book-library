4e4994847a906aef27ffb76eea5a99b9b64922df
sougata <sougataghar47@gmail.com>
2025-04-15 17:51:45 +0530

    First Push

--- src/app/api/addBook/route.js
+++ src/app/api/addBook/route.js
@@ -1,27 +1,27 @@
-import path from 'path';
-import { promises as fs } from 'fs'; 
+import path from "path";
+import { promises as fs } from "fs";
 export async function POST(req) {
-    try{
-    let body = await req.json();
-    let books = [];
-    const filePath = path.join(process.cwd(), 'books.json');
     try {
-        const fileData = await fs.readFile(filePath, 'utf-8');
-        books = JSON.parse(fileData);
-      } catch (readErr) {
-        console.warn('Could not read file.', readErr);
-        return Response.json({ message: 'Error reading file' }, { status: 500 });
-      }
-    books.push({
-        id:String(books.length+1),
-        title:body.title,
-        author:body.author,
-        cover:''})
-    await fs.writeFile(filePath, JSON.stringify(books, null, 2));
-    return Response.json({message:'Book created'});
-}
-    catch(err){
+        let body = await req.json();
+        let books = [];
+        const filePath = path.join(process.cwd(), "books.json");
+        try {
+            const fileData = await fs.readFile(filePath, "utf-8");
+            books = JSON.parse(fileData);
+        } catch (readErr) {
+            console.warn("Could not read file.", readErr);
+            return Response.json({ message: "Error reading file" }, { status: 500 });
+        }
+        books.push({
+            id: String(books.length + 1),
+            title: body.title,
+            author: body.author,
+            cover: "",
+        });
+        await fs.writeFile(filePath, JSON.stringify(books, null, 2));
+        return Response.json({ message: "Book created" });
+    } catch (err) {
         console.error(err);
-        return Response.json({ message: 'Error writing file' }, { status: 500 });
+        return Response.json({ message: "Error writing file" }, { status: 500 });
     }
 }
--- src/app/books/[book]/page.js
+++ src/app/books/[book]/page.js
@@ -1,34 +1,42 @@
 import Link from "next/link";
 import Image from "next/image";
-export default async function Book({params}){
+export default async function Book({ params }) {
     let a = await params;
-    const data = await fetch('http://localhost:3000/api/getBook?id='+a.book)
+    const data = await fetch("http://localhost:3000/api/getBook?id=" + a.book);
     const foundBookbook = await data.json();
-    console.log(foundBookbook)
-    return <div className="text-white grid gap-2 place-items-center">
-        <h1 className="text-4xl">Book Details</h1>
-        {foundBookbook.book.length>0 ?
-        <div className="italic grid gap-2 place-items-center">
-        
-        <Image
-        blurDataURL={'/loadingcover.png'}
-        placeholder="blur" 
-        loading="lazy"
-        alt={foundBookbook.book[0].title}
-        height={320}
-        width={208} 
-        className="object-cover" 
-        src={foundBookbook.book[0].cover===''?'/nocover.png':foundBookbook.book[0].cover}/>
-        
-        <p>Name : {foundBookbook.book[0].title}</p>
-        
-        <p>Author : {foundBookbook.book[0].author}</p>
-    </div> 
-    
-    : <p className="text-white">Book doesn't exist</p>}
-  
-     <Link className="text-white" href={'/search'}> Go back toSearch</Link>
+    console.log(foundBookbook);
+    return (
+        <div className="text-white grid gap-2 place-items-center">
+            <h1 className="text-4xl">Book Details</h1>
+            {foundBookbook.book.length > 0 ? (
+                <div className="italic grid gap-2 place-items-center">
+                    <Image
+                        blurDataURL={"/loadingcover.png"}
+                        placeholder="blur"
+                        loading="lazy"
+                        alt={foundBookbook.book[0].title}
+                        height={320}
+                        width={208}
+                        className="object-cover"
+                        src={
+                            foundBookbook.book[0].cover === ""
+                                ? "/nocover.png"
+                                : foundBookbook.book[0].cover
+                        }
+                    />
 
- 
-    </div>
+                    <p>Name : {foundBookbook.book[0].title}</p>
+
+                    <p>Author : {foundBookbook.book[0].author}</p>
+                </div>
+            ) : (
+                <p className="text-white">Book doesn't exist</p>
+            )}
+
+            <Link className="text-white" href={"/search"}>
+                {" "}
+                Go back to Search
+            </Link>
+        </div>
+    );
 }
